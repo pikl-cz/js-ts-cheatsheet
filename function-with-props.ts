e047e7099d6243f16a6f9f53c73ddaace04baded
Jan Pikl <pikl-cz@users.noreply.github.com>
2023-05-04 13:02:08 +0200

    Update function-with-props.ts

--- function-with-props.ts
+++ function-with-props.ts
@@ -3,8 +3,15 @@ interface MyFunctionProps {
   propertyA?: boolean;
 }
 
-export function myFunction({ propertyA = true }: MyFunctionProps = {}): void {
+// Arrow func
+export function myFunction = ({ propertyA = true }: MyFunctionProps = {}): void {
 
+  if (propertyA) {}
+
+}
+
+// Function
+export function myFunction({ propertyA = true }: MyFunctionProps = {}): void {
 
   if (propertyA) {}
 

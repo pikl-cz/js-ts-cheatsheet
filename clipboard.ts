ca032417540f5c86fa7a3b5ba5cd395f6cf5cba2
Jan Pikl <pikl-cz@users.noreply.github.com>
2023-05-10 13:24:38 +0200

    Update clipboard.ts

--- clipboard.ts
+++ clipboard.ts
@@ -13,3 +13,37 @@ function readFromClipboard() {
     .then((text) => console.log("Clipboard text:", text))
     .catch((err) => console.error("Failed to read from clipboard: ", err));
 }
+
+
+
+// https://stackoverflow.com/questions/51805395/navigator-clipboard-is-undefined
+
+ const copyToClipboard = (text: string) => {
+    if (navigator.clipboard && window.isSecureContext) {
+      navigator.clipboard
+        .writeText(text)
+        .then(() => {
+         console.log("Copied to clipboard")
+        })
+        .catch((err) => console.error("Failed to copy to clipboard: ", err));
+    } else {
+      // Use the 'out of viewport hidden text area' trick
+      const textArea = document.createElement('textarea');
+      textArea.value = text;
+
+      // Move textarea out of the viewport so it's not visible
+      textArea.style.position = 'absolute';
+      textArea.style.left = '-999999px';
+
+      document.body.prepend(textArea);
+      textArea.select();
+
+      try {
+        document.execCommand('copy');
+      } catch (error) {
+        console.error(error);
+      } finally {
+        textArea.remove();
+      }
+    }
+  };

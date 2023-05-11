e70f6b7dbb10b115bf812511c7b1cad804d4fe27
Jan Pikl <pikl-cz@users.noreply.github.com>
2023-05-11 14:37:37 +0200

    Update and rename TODO: shortCircuit.ts to TODO: shortHands.ts

--- TODO: shortHands.ts
+++ TODO: shortHands.ts
@@ -1,2 +1,5 @@
 // Short-circuit evaluation
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_AND#short-circuit_evaluation
+
+// Nullish_coalescing
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing

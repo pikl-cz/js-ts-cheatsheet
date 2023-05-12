2771457d415b407d06d23b516c2170ee705ed880
Jan Pikl <pikl-cz@users.noreply.github.com>
2023-05-12 14:28:26 +0200

    Update functionWithProps.ts

--- functionWithProps.ts
+++ functionWithProps.ts
@@ -16,3 +16,19 @@ export function myFunction({ propertyA = true }: MyFunctionProps = {}): void {
   if (propertyA) {}
 
 }
+
+
+
+// Spread arg. on props + default values
+interface Test {
+  rootMargin?: string;
+  threshold?: number;
+  lazyloadedImages: NodeListOf<HTMLImageElement>;
+  CLASS_SELECTOR: string;
+}
+
+// export const test = (...args: Test[]) => {
+export const test = (args: Test) => {
+  const { lazyloadedImages, rootMargin = '500px', threshold, CLASS_SELECTOR = '.lazyload' } = args;
+  console.log({ lazyloadedImages, rootMargin, threshold, CLASS_SELECTOR });
+};
